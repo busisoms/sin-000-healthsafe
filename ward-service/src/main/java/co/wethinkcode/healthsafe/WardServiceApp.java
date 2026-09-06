4f3f5500108424333ce5629fad44f9dfd525bae6
Busiso <busisondumo1@gmail.com>
2026-09-06 16:34:53 +0200

    Add: GET /wards/{id} endpoint backed by WardClient

--- ward-service/src/main/java/co/wethinkcode/healthsafe/WardServiceApp.java
+++ ward-service/src/main/java/co/wethinkcode/healthsafe/WardServiceApp.java
@@ -2,12 +2,29 @@ package co.wethinkcode.healthsafe;
 
 import io.javalin.Javalin;
 
+import java.util.Map;
+
 public class WardServiceApp {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7031);
+        WardClient client = new WardClient();
+        client.fetchWards();
 
         app.get("/health", ctx -> ctx.result("OK"));
+
+        app.get("/wards/{id}", ctx -> {
+            String id = ctx.pathParam("id");
+           Ward ward = client.findById(id);
+           if (ward == null) {
+               ctx.status(404);
+               ctx.json(Map.of("ERROR", "Ward %s Not Found"
+                       .formatted(id)));
+           }
+           else {
+               ctx.json(ward);
+           }
+        });
 
         // TODO (Provides lists of wards and departments.)
         // Add domain endpoints for ward-service here.
